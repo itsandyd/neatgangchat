739a559202be23d969b99b22a883d4230ad259f0
pauseplayrepeat <86495243+pauseplayrepeat@users.noreply.github.com>
2023-03-20 05:59:27 -0500

    added button
    button for discord

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -4,13 +4,21 @@ import { siteConfig } from "@/config/site"
 
 export function SiteHeader() {
   return (
-    <header className="sticky top-0 z-40 w-full border-b border-b-mauve-7 backdrop-blur-lg   ">
-      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+    <header className="sticky top-0 z-40 w-full border-b border-b-mauve-7 backdrop-blur-lg">
+      <div className="container flex h-16 items-center justify-between">
         <MainNav items={siteConfig.mainNav} />
-        <div className="flex flex-1 items-center justify-end space-x-4">
+        <div className="flex items-center space-x-4">
           <nav className="flex items-center space-x-1">
             <ThemeToggle />
           </nav>
+          <a
+            href="https://discord.gg/neatgang"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="rounded bg-pink-500 px-4 py-2 font-medium text-white hover:bg-pink-600 focus:bg-pink-600 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+          >
+            Join Our Discord
+          </a>
         </div>
       </div>
     </header>
